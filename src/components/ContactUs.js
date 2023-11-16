a568d573e4c4a4a4d749dbdb496a2566f0d2982c
KSU-D2LPP <148568622+KSU-D2LPP@users.noreply.github.com>
2023-11-15 23:05:29 -0500

    Update ContactUs.js
    pictures added

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -6,29 +6,33 @@ const ContactUs = () => {
       <h1>Contact Us</h1>
       
       {/* Team Information */}
-      <div style={{ width: '80%', backgroundColor: 'rgba(0, 0, 0, 0.5)', color: 'gold', padding: '1em' }}>
-        <h3>Project Team</h3>
-        <ul style={{ listStyleType: 'none', padding: 0 }}>
-          <li style={{ marginBottom: '1em' }}>
-            <span>Name: Sean Work</span><br />
-            <span>Email: krownaes@gmail.com</span><br />
-            <span>Role: Developer / Team Lead</span>
-          </li>
-          <li style={{ marginBottom: '1em' }}>
-            <span>Name: Christopher Dymanus</span><br />
-            <span>Email: chris.dymanus@example.com</span><br />
-            <span>Role: Developer</span>
-          </li>
-          <li style={{ marginBottom: '1em' }}>
-            <span>Name: Kenneth Molinari</span><br />
-            <span>Email: kenneth.molinari@example.com</span><br />
-            <span>Role: Developer</span>
-          </li>
-        </ul>
+      <div style={{ display: 'flex', justifyContent: 'space-between', width: '80%', backgroundColor: 'rgba(0, 0, 0, 0.5)', color: 'gold', padding: '1em' }}>
+        <div>
+          <h3>Project Team</h3>
+          <ul style={{ listStyleType: 'none', padding: 0 }}>
+            <li style={{ marginBottom: '1em' }}>
+              <span>Name: Sean Work</span><br />
+              <span>Email: krownaes@gmail.com</span><br />
+              <span>Role: Developer / Team Lead</span>
+            </li>
+            <li style={{ marginBottom: '1em' }}>
+              <span>Name: Christopher Dymanus</span><br />
+              <span>Email: chris.dymanus@example.com</span><br />
+              <span>Role: Developer</span>
+            </li>
+            <li style={{ marginBottom: '1em' }}>
+              <span>Name: Kenneth Molinari</span><br />
+              <span>Email: kenneth.molinari@example.com</span><br />
+              <span>Role: Developer</span>
+            </li>
+          </ul>
+        </div>
+        <img src='/face.jpg' alt='Face' style={{ maxWidth: '200px', maxHeight: '200px', alignSelf: 'center' }} />
       </div>
     </div>
   );
 };
 
 export default ContactUs;
+
 
