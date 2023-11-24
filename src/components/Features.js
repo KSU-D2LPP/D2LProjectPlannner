f7a4d7575552436c56500815549f6a44635f21ca
KSU-D2LPP <148568622+KSU-D2LPP@users.noreply.github.com>
2023-11-24 16:40:27 -0500

    Update Features.js

--- src/components/Features.js
+++ src/components/Features.js
@@ -8,18 +8,7 @@ const Features = () => {
       <h1 className="features-title">Project Features</h1>
       
       <div className="features-box">
-        <h3>D2L Web Application</h3>
-        <p>This project is a React web application that communicates with our self-made API.</p>
-        <h3>User Management</h3>
-        <p>Students and teachers can log in and are identifiable. This prototype is made for our teacher Sharon Perry.</p>
-        <h3>Homepage</h3>
-        <p>The home page has options to return to the home page and login. Users are directed to the login page upon clicking the login button.</p>
-        <h3>Login Options</h3>
-        <p>Users can log in as an admin, student, or create a new account. The database API is currently under construction.</p>
-        <h3>Student Dashboard</h3>
-        <p>Students have the option to select a project and perform peer reviews.</p>
-        <h3>Teacher Dashboard</h3>
-        <p>Teachers have more options including creating a course, importing/exporting data, and viewing reports.</p>
+        
           <div className="youtube-player">
           <iframe 
             src="https://www.youtube.com/embed/zAH14IPaYV8" 
